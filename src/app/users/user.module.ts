5618460627b09243e49554ac54a6352d4882544c
kakashi-p87 <acmilanp7@hotmail.com>
2019-04-09 23:10:15 -0500

    New user form added

--- src/app/users/user.module.ts
+++ src/app/users/user.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { SharedModule } from '../shared/shared.module';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [   
-    UserDetailsComponent
+    UserDetailsComponent, NewUserComponent
   ],
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'user:username', component: UserDetailsComponent }
+      { path: 'user:username', component: UserDetailsComponent },
+      { path: 'new-user', component: NewUserComponent }
     ]),
   ]
 })
